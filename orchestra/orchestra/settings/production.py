4fa8bbbaf0247917b966af0222bcbe696e5fe809
ghdwpaks <hjm7904@gmail.com>
2024-10-17 11:46:46 +0900

    update production setting

--- orchestra/orchestra/settings/production.py
+++ orchestra/orchestra/settings/production.py
@@ -1,27 +1,45 @@
-# production.py
-
 from .base import *
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DEBUG = False
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['your-production-domain.com']
+SECRET_KEY = env('DJANGO_SECRET_KEY', 'fallback-secret-key')
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DB_NAME'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_HOST'),
+        'NAME': env('MYSQL_DB_NAME'),
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASSWORD'),
+        'HOST': env('MYSQL_HOST'),
         'PORT': '3306',
     }
 }
 
 CORS_ALLOWED_ORIGINS = [
-    '15.152.30.9',
+    'http://15.152.30.9',
 ]
 
-SECURE_SSL_REDIRECT = True  # HTTPS 강제 적용
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+# Static files settings (S3 예시)
+STATIC_URL = f'https://{env("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files settings
+MEDIA_URL = f'https://{env("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-west-1')
+AWS_QUERYSTRING_AUTH = False
